d0822ae9bbc3bf4ed9cea7658d00229a8bef11ca
dantecatalfamo <dante.catalfamo@gmail.com>
2026-05-24 16:01:22 -0400

    Use my font, automatically rotate patterns

--- script.js
+++ script.js
@@ -1,57 +1,77 @@
 'use strict'
 
+// const rule = 110;
+
+const set = "\u2588";
+const unset = " ";
+const maxRepeats = 30;
+const linesPerRule = 600;
+const interval = 200;
+
 const auto = document.getElementById("auto");
 const ruleEl = document.getElementById("rule");
 const containerEl = document.getElementById("container");
-const hue = Math.floor((Math.random()  * 1000)%365);
-containerEl.style.color = `hsl(${hue}, 20%, 50%)`;
-ruleEl.style.color = `hsl(${hue}, 20%, 50%)`;
-
-const widthCh = getWidthInCh(auto);
-
-// const rule = 110;
-const rule = Math.floor(Math.random() * 1000 % 256);
-const set = "\u2588";
-const unset = " ";
-const len = Math.floor(widthCh);
-let maxlines = 1;
-let init = new Array(len).fill(unset);
-for (let i = 0; i < len; i++) {
-    if (Math.random() > 0.5) {
-        init[i] = set;
-    }
-}
-auto.innerHTML = init.join("");
-ruleEl.innerText = `Rule ${rule}`;
-document.title = `Rule ${rule}`;
 
 let tab = [];
-for (let i = 0; i < 8; i++) {
-    tab[i] = (rule>>i)&1
+let maxLines;
+let lineNumber;
+let repeats;
+
+function initRule(rule) {
+    maxLines = 1;
+    lineNumber = 0;
+    repeats = 0;
+    tab = [];
+    for (let i = 0; i < 8; i++) {
+        tab[i] = (rule >> i) & 1
+    }
+
+    let hue = Math.floor((Math.random()  * 1000)%365);
+    containerEl.style.color = `hsl(${hue}, 20%, 50%)`;
+    ruleEl.style.color = `hsl(${hue}, 20%, 50%)`;
+
+    const widthCh = Math.floor(getWidthInCh(auto));
+    let init = new Array(widthCh).fill(unset);
+    for (let i = 0; i < widthCh; i++) {
+        if (Math.random() > 0.5) {
+            init[i] = set;
+        }
+    }
+
+    auto.innerHTML = init.join("");
+    ruleEl.innerText = `Rule ${rule}`;
+    document.title = `Rule ${rule}`;
 }
 
 function iterRule() {
+    if (lineNumber++ > linesPerRule) {
+        console.log("linesPerRule");
+        newRule();
+    }
+
     if (!enoughLines()) {
-        maxlines++;
+        maxLines++;
     }
     const text = auto.innerText;
     let lines = text.split("\n");
     const chars = lines[lines.length-1].split('');
-    let newline = [];
+    let newchars = [];
     for (let chr = 0; chr < chars.length; chr++) {
         const prev = (chars[chr-1] && chars[chr-1] != unset && 1) || 0;
         const cur = (chars[chr] && chars[chr] != unset && 1) || 0;
         const next = (chars[chr+1] && chars[chr+1] != unset && 1) || 0;
         const idx = (prev<<2) | (cur<<1) | next;
-        newline[chr] = tab[idx] ? set : unset;
+        newchars[chr] = tab[idx] ? set : unset;
     }
-    lines.push(newline.join(""));
-    lines = lines.slice(Math.max(lines.length-maxlines, 0));
+    const newline = newchars.join("");
+    if (newline == lines[lines.length-1] && repeats++ > maxRepeats) {
+        console.log("maxReleats");
+        newRule();
+    }
+    lines.push(newline);
+    lines = lines.slice(Math.max(lines.length-maxLines, 0));
     auto.innerText = lines.join("\n");
 }
-
-setInterval(iterRule, 200);
-console.log("rule", rule);
 
 function getWidthInCh(element) {
     const ruler = document.createElement('div');
@@ -76,3 +96,10 @@ function getWidthInCh(element) {
 function enoughLines() {
     return Math.floor(auto.getBoundingClientRect().height) > window.innerHeight;
 }
+
+function newRule() {
+    initRule(Math.floor(Math.random() * 1000 % 256));
+}
+
+newRule();
+setInterval(iterRule, interval);
